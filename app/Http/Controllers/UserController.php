46fa66f6c9c5573fb0dbb299213f224e25dc096f
andrem-1230 <jungerbard@gmail.com>
2024-02-25 12:21:22 +0300

    Initial commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        //return User::all();
+        return response()->json(['message' => "Миша, привет!"], 201);
+    }
+
+    public function store(Request $request)
+    {
+        //$user = User::create($request->all());
+
+        return response()->json("123", 201);
+    }
+
+   /* public function update(Request $request, User $user)
+    {
+        $user->update($request->all());
+
+        return response()->json($user);
+    }
+
+    public function destroy(User $user)
+    {
+        $user->delete();
+
+        return response()->json(null, 204);
+    }*/
+}
